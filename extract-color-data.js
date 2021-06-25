f7d6813fa4d40190e5c12c6327aab453995629cd
vitor rodrigues <vitorsilva.com@gmail.com>
2021-06-25 15:31:30 +0100

    second test with get colors

--- extract-color-data.js
+++ extract-color-data.js
@@ -1,5 +1,6 @@
 /*
-    base example using node https://lokeshdhakar.com/projects/color-thief/ 
+    -- base example using node https://lokeshdhakar.com/projects/color-thief/ 
+    -- second test with all color
 */
 
 const ColorThief = require('colorthief');
@@ -10,11 +11,45 @@ var resolve = require('resolve');
 const img = 'rainbow.jpg'
 console.log(img);
 
+var a;
 
-ColorThief.getColor(img)
-    .then(color => { console.log(color) })
-    .catch(err => { console.log(err) })
+async function start() {
 
-ColorThief.getPalette(img, 5)
-    .then(palette => { console.log(palette) })
-    .catch(err => { console.log(err) })
+    var imgs = ["rainbow.jpg"];
+    var totalColors = [];
+
+    for (let i = 0; i < imgs.length; i++) {
+
+        var colors = await getColors(imgs[i]);
+        totalColors.push({
+            img: imgs[i],
+            colors: colors
+        });
+    }
+
+    console.log("TotalColors:");
+    console.table(totalColors[0].colors);
+
+    //SAVE
+
+}
+
+async function getColors(img) {
+    var result = [];
+    var dominantColor = await ColorThief.getColor(img);
+    result.push(dominantColor);
+
+    var palette = await ColorThief.getPalette(img, 5)
+    result.push(...palette);
+    return result;
+}
+
+start();
+
+//console.log(a);
+
+
+
+
+// nomeficheiro, cordominante    
+// nomeficheiro, paleta1, paleta12, ....
